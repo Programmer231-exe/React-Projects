6ba8357876a05c0e99e995e11d1682b4d24ce9f4
Programmer231-exe <63851580+Programmer231-exe@users.noreply.github.com>
2020-12-07 14:08:21 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import DateChecker from "./Components/dateChecker";
+
+function App() {
+  return (
+    <div className="App">
+     <DateChecker />
+    </div>
+  );
+}
+
+export default App;
